bade9766e98fa49225232d7dd31ef8bb02dfdb5d
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-06-11 23:15:21 -0500

    Display image cards

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -29,20 +29,202 @@ class _catalogState extends State<catalog> {
                   Container(
                     height: 200,
                     margin: const EdgeInsets.all(10),
-                    child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.amber,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 120,
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(20),
+                            image: const DecorationImage(
+                              image: AssetImage("assets/images/food1.jpg"),
+                              fit: BoxFit.cover
+                            )
+                          ),
+                        ),
+                        Container(
+                          width: 200,
+                          margin: const EdgeInsets.all(10),
+                          child: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Producto: Ensalada de zanahoria',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Precio: S/ 25.00',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white
+                                ),
+                              )
+                            ]
+                          ),
+                        )
+                      ]
+                    )
+                  ),
+                  Container(
+                      height: 200,
+                      margin: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                                color: Colors.black26,
-                                offset: Offset(5.0, 5.0),
-                                blurRadius: 5.0
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.amber,
+                      ),
+                      child: Row(
+                          children: [
+                            Container(
+                              width: 120,
+                              margin: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: const DecorationImage(
+                                      image: AssetImage("assets/images/food2.jpg"),
+                                      fit: BoxFit.cover
+                                  )
+                              ),
+                            ),
+                            Container(
+                              width: 200,
+                              margin: const EdgeInsets.all(10),
+                              child: const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Producto: Ensalada de huevo',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Precio: S/ 17.50',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    )
+                                  ]
+                              ),
                             )
                           ]
+                      )
+                  ),
+                  Container(
+                      height: 200,
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.amber,
                       ),
-                      padding: const EdgeInsets.all(10),
-                    )
+                      child: Row(
+                          children: [
+                            Container(
+                              width: 120,
+                              margin: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: const DecorationImage(
+                                      image: AssetImage("assets/images/food3.jpg"),
+                                      fit: BoxFit.cover
+                                  )
+                              ),
+                            ),
+                            Container(
+                              width: 200,
+                              margin: const EdgeInsets.all(10),
+                              child: const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Producto: Ensalada Mixta',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Precio: S/ 18.00',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    )
+                                  ]
+                              ),
+                            )
+                          ]
+                      )
+                  ),
+                  Container(
+                      height: 200,
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.amber,
+                      ),
+                      child: Row(
+                          children: [
+                            Container(
+                              width: 120,
+                              margin: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(20),
+                                  image: const DecorationImage(
+                                      image: AssetImage("assets/images/food4.jpg"),
+                                      fit: BoxFit.cover
+                                  )
+                              ),
+                            ),
+                            Container(
+                              width: 200,
+                              margin: const EdgeInsets.all(10),
+                              child: const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Producto: Tacos',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Precio: S/ 35.00',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white
+                                      ),
+                                    )
+                                  ]
+                              ),
+                            )
+                          ]
+                      )
                   ),
                 ],
               )
